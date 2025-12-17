20008a12b405de4db9dfaa5f86ac081dd16192ca
Akhmetzhar <241146@astanait.edu.kz>
2025-12-17 18:28:09 +0500

    Delete week-1 directory

--- week-1/task3.py
+++
@@ -1,4 +0,0 @@
-a=float(input())
-i=int(a)
-f=int(round((a-i)*100))
-print(f+i/100)
--- week-1/task9.py
+++
@@ -1,2 +0,0 @@
-ticket = input()
-print("YES" if sum(map(int, ticket[:3])) == sum(map(int, ticket[3:])) else "NO")
